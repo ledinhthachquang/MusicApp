df22d51e7a5eea743d4b7f55b9b77dafb2c7de52
z0mbie-users <20522009@gm.uit.edu.vn>
2023-06-02 17:09:49 +0700

    Add Fragment voi Mode

---
+++ app/src/main/java/com/example/appnhac/Fragment/Fragment_Playlist.java
@@ -0,0 +1,26 @@
+package com.example.appnhac.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.appnhac.R;
+
+
+public class Fragment_Playlist extends Fragment {
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,  @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_playlist, container, false);
+        return view;
+    }
+}
