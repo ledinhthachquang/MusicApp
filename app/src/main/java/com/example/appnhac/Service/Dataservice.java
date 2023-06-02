a6e09697698c36c184954b4c6d89e4e0bd7b0824
z0mbie-users <20522009@gm.uit.edu.vn>
2023-06-02 16:43:42 +0700

    Update Service

--- app/src/main/java/com/example/appnhac/Service/Dataservice.java
+++ app/src/main/java/com/example/appnhac/Service/Dataservice.java
@@ -1,13 +1,15 @@
 package com.example.appnhac.Service;
 
 import com.example.appnhac.Model.Quangcao;
-
+import com.example.appnhac.Model.Playlist;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Dataservice {
-    @GET("getBanner.php?fbclid=IwAR0NyKDtX76wyE642r8wTNtKPbP6NwDBwm02NiCCe6fhd5Fp0KA1sYuDdQ0")
+    @GET("getBanner.php")
     Call<List<Quangcao>> GetDataBanner();
+    @GET("random_playlist.php")
+    Call<List<Playlist>> GetPlaylist();
 }
